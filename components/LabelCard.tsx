f2d35534938c6fb3e6f27bd3994832e5a27c0a3f
Jason Zubiate <jzubiate.dev@gmail.com>
2024-03-03 12:17:14 -0800

    refactor: adds image sizes property

--- components/LabelCard.tsx
+++ components/LabelCard.tsx
@@ -6,19 +6,25 @@ import { LabelCardProps } from "@/types";
 
 export default function LabelCard({ label }: LabelCardProps) {
   return (
-    <a className="label-card" onClick={() => redirectToLabel(label)}>
+    <div className="label-card" onClick={() => redirectToLabel(label)}>
       <h4 className="text-lg lg:text-[48px] uppercase font-semibold lg:tracking-tight">
         {label.name}
       </h4>
 
       <div className="label-card-img">
-        <Image src={label.img} alt={label.name} quality={100} fill />
+        <Image
+          src={label.img}
+          alt={label.name}
+          fill
+          priority
+          sizes="(max-width: 1024px) 150px, 200px"
+        />
       </div>
 
       <div className="flex flex-col items-end text-[10px] lg:text-[14px] leading-none lg:leading-tight">
         <p className="uppercase ">{label.owner}</p>
         <p className="text-[#8e8b86] ">Accepting</p>
       </div>
-    </a>
+    </div>
   );
 }
