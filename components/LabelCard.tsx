d264adbc19294799756bcdd598d5927055e862ba
Jason Zubiate <jzubiate.dev@gmail.com>
2024-03-03 19:55:17 -0800

    refactor: LabelCard style changes

--- components/LabelCard.tsx
+++ components/LabelCard.tsx
@@ -7,9 +7,7 @@ import { LabelCardProps } from "@/types";
 export default function LabelCard({ label }: LabelCardProps) {
   return (
     <div className="label-card" onClick={() => redirectToLabel(label)}>
-      <h4 className="text-lg lg:text-[48px] uppercase font-semibold lg:tracking-tight">
-        {label.name}
-      </h4>
+      <h4>{label.name}</h4>
 
       <div className="label-card-img">
         <Image
@@ -21,9 +19,9 @@ export default function LabelCard({ label }: LabelCardProps) {
         />
       </div>
 
-      <div className="flex flex-col items-end text-[10px] lg:text-[14px] leading-none lg:leading-tight">
+      <div className="label-card-details">
         <p className="uppercase ">{label.owner}</p>
-        <p className="text-[#8e8b86] ">Accepting</p>
+        <p className="text-ashen-taupe ">Accepting</p>
       </div>
     </div>
   );
