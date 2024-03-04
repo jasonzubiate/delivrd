f86dd0f02a9e9cd51a8787547980dd8b3f3f74ca
Jason Zubiate <jzubiate.dev@gmail.com>
2024-03-04 14:14:43 -0800

    refactor: style changes

--- components/Labels.tsx
+++ components/Labels.tsx
@@ -3,7 +3,7 @@ import LabelCardList from "./LabelCardList";
 export default function Labels() {
   return (
     <section id="labels" className="pt-24">
-      <h2 className="font-medium uppercase text-2xl px-2 md:px-6 mb-8">
+      <h2 className="font-medium uppercase text-lg md:text-2xl px-4 md:px-6 mb-8">
         Featured Labels
       </h2>
       <LabelCardList />
