c4aa5ad334264736218a247ad58dafc281add292
Jason Zubiate <jzubiate.dev@gmail.com>
2024-03-04 16:37:51 -0800

    refactor: style changes"

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,25 +2,25 @@ export default function Footer() {
   return (
     <footer
       id="footer"
-      className="flex w-full justify-between px-4 py-6 md:p-6"
+      className="flex w-full justify-between items-center px-4 py-6 md:p-6"
     >
       <div className="flex gap-4">
         <a
           href="https://instagram.com/delivrdlive"
           target="_blank"
-          className="font-light"
+          className="font-light text-sm md:text-base"
         >
           Instagram
         </a>
         <a
           href="https://jasonzubiate.com"
           target="_blank"
-          className="font-light"
+          className="font-light text-sm md:text-base"
         >
           Creator
         </a>
       </div>
-      <p className="text-ashen-taupe font-light">
+      <p className="text-ashen-taupe font-light text-xs md:text-sm">
         DELIVRD © {new Date().getFullYear()}
       </p>
     </footer>
